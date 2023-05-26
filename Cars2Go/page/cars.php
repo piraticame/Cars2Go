1b10ea9bceb65747a87899aafd077df8610d2743
Howard Glen Gloria <89847338+piraticame@users.noreply.github.com>
2023-05-26 18:38:42 +0800

    Add files via upload

---
+++ Cars2Go/page/cars.php
@@ -0,0 +1,60 @@
+<?php
+require '../database/config.php';
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+   
+    <title>Cars2Go</title>
+  </head>
+<body>
+   <header>
+    <?php
+    include ('header.php');
+   ?>
+   </header>
+   <br>
+   <br>
+    
+   <div class="main-container">
+   <?php
+// Assuming you have established a database connection
+
+// Fetch data from the SQL table
+$query = "SELECT * FROM car";
+$result = mysqli_query($conn, $query);
+
+// Loop through the fetched rows to generate clickable divs
+while ($row = mysqli_fetch_assoc($result)) {
+  $id = $row['CarID'];
+  $carName = $row['CarName'];
+  $PlateNumber = $row['PlateNumber'];
+  $ACperDay = $row['ACperDay'];
+  $ACperKm = $row['ACperKm'];
+  $NonACperDay = $row['NonACperDay'];
+  $NonACperKm = $row['NonACperKm'];
+  $CarImg = $row['CarImg'];
+
+  // Generate the HTML for each clickable div with a link to order.php
+  echo "<div class='car-div' id='div$id'>";
+  echo "<a href='order.php?id=$id'>";
+  echo "<img src='../img/$CarImg' alt=''>";
+  echo "<h3><i>$carName</i></h3>";
+  echo "<p>Plate Number: $PlateNumber</p>";
+  echo "<p>AC per Day:  P$ACperDay</p>";
+  echo "<p>AC per Km: P$ACperKm</p>";
+  echo "<p>Non AC per Day: P$NonACperDay</p>";
+  echo "<p>Non AC per Km: P$NonACperKm</p>";
+  echo "</a>";
+  echo "</div>";
+}
+
+?>
+   </div>
+
+</body>
+</html>
---
+++ Cars2Go/page/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <link rel="stylesheet" href="../css/style.css">
+      <script src="../js/script.js" defer></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Monomaniac+One&display=swap" rel="stylesheet">
+      <title>Responsive Navbar</title>
+    </head>
+    <body>
+      <nav class="navbar">
+        <div class="brand-title">
+            <img src="../img/logos.png" alt="">
+        </div>
+        <a href="#" class="toggle-button">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+        <div class="navbar-links">
+          <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="cars.php">See Cars</a></li>
+            <li><a href="#">Faqs</a></li>
+          </ul>
+        </div>
+      </nav>
+    </body>
+</html>
